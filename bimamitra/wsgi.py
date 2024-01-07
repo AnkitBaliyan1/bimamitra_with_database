27f3ecb88ea970a916973076cad93797b70a0517
Ankit Baliyan <86275544+AnkitBaliyan1@users.noreply.github.com>
2024-01-07 13:37:59 +0530

    Update wsgi.py

--- bimamitra/wsgi.py
+++ bimamitra/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'bimamitra.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'bimamitra.settings'
+settings_module = 'bimamitra.deployment' if 'DEPLOYMENT_FLAG' in os.environ else 'bimamitra.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
