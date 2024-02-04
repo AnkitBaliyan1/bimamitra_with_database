4ffaa2dcdc324f9b8f5d9e0b2251f463cf95d8c1
ankit <a.baliyan008@gmail.com>
2024-02-04 18:28:44 +0530

    update BASE_DIR

--- manage.py
+++ manage.py
@@ -11,6 +11,7 @@ def main():
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bimamitra.deployment')
     else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bimamitra.settings')
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
